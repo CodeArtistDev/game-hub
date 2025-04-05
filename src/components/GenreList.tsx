18476a2b61a2bb65f88ce85df0b64a722581bfad
Code Artist Dev <codeartistdev@gmail.com>
2025-04-05 18:20:19 +0530

    Cleanup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/crop-image";
-import { Box, Button, HStack, Image, Link, Spinner} from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Image, Link, Spinner, Text } from "@chakra-ui/react";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -9,22 +9,39 @@ interface Props {
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
-  if(error) 
-    return null;
-  if(isLoading)
-    return <Spinner />
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <Box as="ul" listStyleType="none">
       {data.map((genre) => (
-        <li key={genre.id} >
-          <HStack paddingY='5px'>
-            <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold': ''} onClick={() => onSelectGenre(genre)} asChild variant='ghost' fontSize='lg'><Link href='#' variant='plain'>{genre.name}</Link></Button>
+        <li key={genre.id}>
+          <HStack paddingY="5px">
+            <Image
+              objectFit="cover"
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : ""}
+              onClick={() => onSelectGenre(genre)}
+              asChild
+              variant="ghost"
+              fontSize="lg"
+            >
+              <Link
+                href="#"
+                variant="plain"
+              >{genre.name}
+              </Link>
+            </Button>
           </HStack>
         </li>
       ))}
     </Box>
+    </>
   );
 };
 
